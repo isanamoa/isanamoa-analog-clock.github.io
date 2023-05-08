e11af93e32f2da614eddbd6744802f0170886c81
isanamoa <isaacansaha@gmail.com>
2023-05-08 10:47:34 +0000

    Analog Clock Script Updated

--- script.js
+++ script.js
@@ -15,11 +15,11 @@ const clockFunc = () => {
 
     console.log(hourAng, minuteAng, secondsAng);
 
-    document.getElementById('hours').setAttribute('transform', `rotate(${hourAng} 245, 250)`);
-    document.getElementById('minutes').setAttribute('transform', `rotate(${minuteAng} 245, 250)`);
-    document.getElementById('seconds').setAttribute('transform', `rotate(${secondsAng} 245, 250)`);
+    document.getElementById('hours').setAttribute('transform', `rotate(${hourAng} 244, 250)`);
+    document.getElementById('minutes').setAttribute('transform', `rotate(${minuteAng} 244, 250)`);
+    document.getElementById('seconds').setAttribute('transform', `rotate(${secondsAng} 243, 250)`);
 
 }
-//clockFunc()
+clockFunc()
 // Update function for updating the clock handles per second
 setInterval(() => clockFunc(), 1000);
