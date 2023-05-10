df6509732b7721e2a30481ed10d8a153459ddb1a
isanamoa <isaacansaha@gmail.com>
2023-05-10 15:46:36 +0000

    Analog Clock Script Updated 2

--- script.js
+++ script.js
@@ -8,16 +8,18 @@ const clockFunc = () => {
         seconds = myDate.getSeconds();
 
     console.log(hour, minute, seconds);
-
+    // To get rotation angle, calc the number of degree/per second, per minute and per hour
     const secondsAng = seconds * 6,
         minuteAng = minute * 6,
+        //Alt A
         hourAng = hour * 30 + (minute / 2);
+        // Alt B
+        //hourAng = hour > 12 ? (hour -12) * 30 : hour * 30;
+    //console.log(hourAng, minuteAng, secondsAng);
 
-    console.log(hourAng, minuteAng, secondsAng);
-
-    document.getElementById('hours').setAttribute('transform', `rotate(${hourAng} 244, 250)`);
-    document.getElementById('minutes').setAttribute('transform', `rotate(${minuteAng} 244, 250)`);
-    document.getElementById('seconds').setAttribute('transform', `rotate(${secondsAng} 243, 250)`);
+    document.getElementById('hours').setAttribute('transform', `rotate(${hourAng} 244, 251)`);
+    document.getElementById('minutes').setAttribute('transform', `rotate(${minuteAng} 244, 251)`);
+    document.getElementById('seconds').setAttribute('transform', `rotate(${secondsAng} 244, 251)`);
 
 }
 clockFunc()
